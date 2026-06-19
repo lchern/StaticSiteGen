6f007b52ee52691b4325710e103e01eab0b1da7e
lchern <leonid.chern@gmail.com>
2026-06-19 15:46:52 +0400

    19.06.2026 added LeafNode and 1 test

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -11,6 +11,10 @@ class TestHTMLNode(unittest.TestCase):
         actual_result = node.props_to_html()
         expected_result = ' href="https://www.google.com" target="_blank"'
         self.assertEqual(actual_result, expected_result)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
 if __name__ == "__main__":
     unittest.main()
